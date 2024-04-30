80a185b1a8e23e53f702fe5ed6ffd2475edbaa7e
yu-to06 <shun6.u@gmail.com>
2024-04-30 23:28:43 +0900

    edited Hero.js line 38 to line 83

--- components/Hero.js
+++ components/Hero.js
@@ -1,5 +1,9 @@
+"use client";
+
 import React from "react";
 import Header from "./Header";
+import CountUp from "react-countup";
+import {MdOutlineSportsTennis} from "react-icons/md";
 
 function Hero() {
   return (
@@ -33,23 +37,51 @@ function Hero() {
         {/* 軽い実績 */}
         <div className='flex gap-[2rem] pt-[2rem] text-center'>
           <div className='flex flex-col'>
-            <span className='text-white text-3xl'>140人</span>
+            <span className='text-white text-3xl'>
+              <CountUp start={100} end={140} duration={4} />人
+            </span>
             <p className='text-gray'>在籍コーチ</p>
           </div>
           <div>
-            <span className='text-white text-3xl'>5978名</span>
+            <span className='text-white text-3xl'>
+              <CountUp start={5000} end={5978} duration={4} />名
+            </span>
             <p className='text-gray'>会員数</p>
           </div>
           <div>
-            <span className='text-white text-3xl'>62施設</span>
+            <span className='text-white text-3xl'>
+              <CountUp start={10} end={62} duration={4} />
+              施設
+            </span>
             <p className='text-gray'>店舗数</p>
           </div>
         </div>
         {/* 案内 */}
+        <div className='flex gap-[1rem] font-normal pt-[1rem]'>
+          <a
+            href=''
+            className='text-white bg-pink w-[8rem] p-[0.5rem] font-bold text-center'
+          >
+            会員
+          </a>
+          <a
+            href=''
+            className='text-white bg-transparent w-[8rem] p-[0.5rem] border-[4px] font-bold text-centers'
+          >
+            体験
+          </a>
+        </div>
       </div>
       {/* 右 */}
-      <div className='col-span-4'>
+      <div className='col-span-4 relative bg-gradient-to-br from-pink via-pink to-yellow-500'>
         {/* レート */}
+
+        <div className='absolute flex flex-col gap-[1rem] bg-darkGray w-fit p-[1rem] text-start rounded-md right-[4rem] top-[8rem]'>
+          <MdOutlineSportsTennis className='text-5xl text-pink' />
+          <span className='text-gray '>UTR Rate</span>
+          <span className='text-white text-2xl'>9.5</span>
+        </div>
+
         {/* メイン写真 */}
         {/* 背景写真 */}
       </div>
