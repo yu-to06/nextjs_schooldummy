c8212e69ba1d467fb222f6e26f6b7058070f3b7e
yu-to06 <shun6.u@gmail.com>
2024-05-15 23:16:56 +0900

    Finished Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import {FaTwitter, FaYoutube} from "react-icons/fa";
+import {IoLogoInstagram} from "react-icons/io";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className='relative mb-[5rem]'>
+      <hr className='border-[1px] border-lightGray' />
+      <div className='flex flex-col py-[1rem] px-[2rem] gap-[4rem] items-center justify-center h-[20rem]'>
+        <div className='flex gap-[4rem]'>
+          <a href=''>
+            <FaTwitter className='text-4xl text-white' />
+          </a>
+          <a href=''>
+            <IoLogoInstagram className='text-4xl text-white' />
+          </a>
+          <a href=''>
+            <FaYoutube className='text-4xl text-white' />
+          </a>
+        </div>
+        <div>
+          <Image
+            src={"/assets/logo.png"}
+            width={200}
+            height={100}
+            alt={"logo"}
+            className='w-[10rem]'
+          />
+        </div>
+      </div>
+      {/* ブラー */}
+      <div
+        className='blur bottom-0 right-[15%] w-[26rem] h-[12rem]'
+        style={{background: "red"}}
+      />
+      <div className='blur bottom-0 left-[15%] w-[26rem] h-[12rem]' />
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- components/Join.js
+++ components/Join.js
@@ -5,12 +5,13 @@ import emailjs from "@emailjs/browser";
 function Join() {
   const form = useRef();
 
+  //EmailJsの情報は一旦消去
   const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs
-      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", form.current, {
-        publicKey: "YOUR_PUBLIC_KEY",
+      .sendForm("SERVICE_CONNECTID", "TEMPLATE_ID", form.current, {
+        publicKey: "PUBLICKEY",
       })
       .then(
         () => {
@@ -24,9 +25,16 @@ function Join() {
   };
 
   return (
-    <div className='flex px-[2rem] gap-[10rem]' id='join'>
+    <div
+      className='flex px-[2rem] gap-[10rem] 
+      max-md:flex-col max-md:gap-[1rem]'
+      id='join'
+    >
       {/* left */}
-      <div className='text-white font-bold relative flex flex-col gap-[2rem] text-5xl'>
+      <div
+        className='text-white font-bold relative flex flex-col gap-[2rem] text-5xl
+      max-md:text-3xl'
+      >
         <hr className='absolute border-[2px] text-pink w-[11rem] rounded-full -m-[10px]' />
         <div>
           <span className='storoke-text'>いますぐ</span>
@@ -39,19 +47,27 @@ function Join() {
       </div>
 
       {/* right  */}
-      <div className='flex justify-center items-end'>
+      <div
+        className='flex justify-center items-end
+      max-md:p-[2rem]'
+      >
         <form
           ref={form}
           onSubmit={sendEmail}
-          className='flex gap-[3rem] bg-gray py-[1rem] px-[2rem]'
+          className='flex gap-[3rem] bg-gray py-[1rem] px-[2rem]
+          max-md:w-[20rem]'
         >
           <input
             type='email'
             name='user_email'
             placeholder='Email Address'
-            className='bg-transparent border-none outline-none bg-white text-center placeholder:text-lightGray'
+            className='bg-transparent border-none outline-none bg-white text-center placeholder:text-lightGray
+            max-md:p-0 max-md:w-[10rem]'
           />
-          <button className='w-[6rem] text-white bg-pink p-[0.5rem] border-[2px] border-transparent font-bold'>
+          <button
+            className='w-[6rem] text-white bg-pink p-[0.5rem] border-[2px] border-transparent font-bold
+          max-md:min-w-[4rem] max-md:text-sm max-md:mr-[2rem]'
+          >
             Join
           </button>
         </form>
